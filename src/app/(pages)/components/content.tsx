ed7e1d7d3b190f5a095913e448a8af413d96d9d8
Mouzone <sunnyliu010@gmail.com>
2024-11-15 18:37:04 -0500

    Minor changes

--- src/app/(pages)/components/content.tsx
+++
@@ -1,9 +0,0 @@
-export default function Content() {
-    return (
-        <div className="bg-white mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-            <h2 className="text-3xl"> This Is </h2>
-            <h2> My </h2>
-            <h2> Blog </h2>
-        </div>
-    )
-}
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,12 +1,3 @@
-import Navbar from "@/app/(pages)/components/Navbar.tsx";
-import Content from "@/app/(pages)/components/content.tsx";
-
-export default function Landing() {
-    // todo: add logic here to check if access token
-    return (
-        <>
-            <Navbar/>
-            <Content/>
-        </>
-    )
+export default function Home() {
+    const data = fetch("/api/posts")
 }
