c9ba8822bad51aa24fe0609ca1a096252a634011
Mouzone <sunnyliu010@gmail.com>
2024-11-15 18:21:28 -0500

    Create front end components

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,23 +1,12 @@
 import Navbar from "@/app/(pages)/components/Navbar.tsx";
+import Content from "@/app/(pages)/components/content.tsx";
 
 export default function Landing() {
     // todo: add logic here to check if access token
     return (
         <>
             <Navbar/>
-            <div className="bg-amber-50">
-                <div>
-                    This is
-                </div>
-                <div>
-                    My
-                </div>
-                <div>
-                    Blog
-                </div>
-            </div>
-
+            <Content/>
         </>
-
     )
 }
