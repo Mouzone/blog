14b116742f46a116bfa09a93cd1967509a838da4
Mouzone <sunnyliu010@gmail.com>
2024-11-15 19:22:32 -0500

    Refactor routing segments

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,5 +1,5 @@
 import {headers} from "next/headers";
-import {createComment, findComments} from "../../../../../../prisma/commentQueries.ts";
+import {createComment, findComments} from "../../../../prisma/commentQueries.ts";
 
 export async function GET(_request: Request, { params }: { params: Promise<{ postId: string }> }) {
     const postId = (await params).postId
--- src/app/api/log-in/route.ts
+++ src/app/api/log-in/route.ts
@@ -1,4 +1,4 @@
-import { getToken } from "../../../../../utility/getToken.ts";
+import { getToken } from "../../../../utility/getToken.ts";
 
 export async function POST(request: Request) {
     const body = await request.json()
