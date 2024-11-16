b4f7a7a0920ad7e887db6814dda652b86e36a8ac
Mouzone <sunnyliu010@gmail.com>
2024-11-15 19:14:46 -0500

    Rewrite log-in route to work with hardcoded credentials and remove sign-up route

--- src/app/api/(account)/log-in/route.ts
+++ src/app/api/(account)/log-in/route.ts
@@ -1,54 +1,33 @@
-import { findProfileByUsername } from "../../../../../prisma/profileQueries.ts";
 import { getToken } from "../../../../../utility/getToken.ts";
 
 export async function POST(request: Request) {
-    try {
-        const body = await request.json()
-        const { username, password } = body
+    const body = await request.json()
+    const { username, password } = body
 
-        const profile = await findProfileByUsername(username)
-        if (!profile) {
-            return Response.json({
-                error: true,
-                status: 404,
-                message: 'Username does not exist'
-            })
-        }
-
-        const isMatch = await Bun.password.verify(password, profile.password)
-        if (!isMatch) {
-            return Response.json({
-                error: true,
-                status: 401,
-                message: 'Password is incorrect'
-            })
-        }
-
-        try {
-            const accessToken = await getToken({ id: profile.id }, "1h")
-            const refreshToken = await getToken( {id: profile.id})
-
-            return Response.json({
-                error: false,
-                status: 200,
-                accessToken,
-                refreshToken,
-                message: "success"
-            })
-        } catch(error) {
-            return Response.json({
-                error: true,
-                status: 503,
-                message: `Error generating token ${error}`
-            })
-        }
-
-    } catch (error) {
-        console.error('Login error:', error)
+    if (username !== process.env["USERNAME"] && password !== process.env["PASSWORD"]) {
         return Response.json({
             error: true,
-            status: 500,
-            message: 'Internal server error'
+            status: 401,
+            message: 'Invalid credentials'
+        })
+    }
+
+    try {
+        const accessToken = await getToken({ }, "1h")
+        const refreshToken = await getToken( { })
+
+        return Response.json({
+            error: false,
+            status: 200,
+            accessToken,
+            refreshToken,
+            message: "success"
+        })
+    } catch(error) {
+        return Response.json({
+            error: true,
+            status: 503,
+            message: `Error generating token ${error}`
         })
     }
 }
