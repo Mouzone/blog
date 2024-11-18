2ee959716e3e671c7864bfb5aacb4c6501944d27
Mouzone <sunnyliu010@gmail.com>
2024-11-18 15:06:16 -0500

    Add data fetching of posts and render it on blogs

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,35 +1,32 @@
-const posts = [
-    {
-        id: 1,
-        title: 'Boost your conversion rate',
-        href: '#',
-        description:
-            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-        date: 'Mar 16, 2020',
-        datetime: '2020-03-16',
-    },
-    // More posts...
-]
+interface Post {
+    id: string,
+    title: string,
+    description: string,
+    createdAt: string,
+    isShown: string,
+}
+export default async function Posts() {
+    const response = await fetch("http://localhost:3000/api/posts")
+    const data: { posts: Post[] } = await response.json()
+    const posts = data["posts"]
 
-export default function Posts() {
     return (
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">From the blog</h2>
-                    <p className="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+                    <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">My Blog</h2>
                 </div>
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {posts.map((post) => (
                         <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
                             <div className="flex items-center gap-x-4 text-xs">
-                                <time dateTime={post.datetime} className="text-gray-500">
-                                    {post.date}
+                                <time dateTime={post.createdAt} className="text-gray-500">
+                                    {post.createdAt}
                                 </time>
                             </div>
                             <div className="group relative">
                                 <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                                    <a href={post.href}>
+                                    <a href={post.title}>
                                         <span className="absolute inset-0" />
                                         {post.title}
                                     </a>
