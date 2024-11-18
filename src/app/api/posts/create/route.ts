aa15e700a93043dde520511ac9ac44bac586f9a7
Mouzone <sunnyliu010@gmail.com>
2024-11-18 12:09:08 -0500

    Add an id on return message for creating posts for testing purposes

--- src/app/api/posts/create/route.ts
+++ src/app/api/posts/create/route.ts
@@ -3,9 +3,10 @@ import {createPost} from "../../../../../prisma/postQueries.ts";
 export async function POST(request: Request) {
     const { title, content } = await request.json()
 
-    await createPost(title, content)
+    const {id} = await createPost(title, content)
     return Response.json({
         error: false,
+        postId: id,
         status: 200,
         message: "success"
     })
