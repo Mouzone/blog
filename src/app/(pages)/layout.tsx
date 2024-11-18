ef452895587ac256bad20b88ee7d06e611d2e272
Mouzone <sunnyliu010@gmail.com>
2024-11-18 14:46:39 -0500

    Add basic tailwind components for navbar and the posts page

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
+import NavBar from "@/app/(pages)/NavBar.tsx";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
+        <NavBar/>
         {children}
       </body>
     </html>
