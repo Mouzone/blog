511f862985f471868c9dcb6688e32fb92d8df727
Mouzone <sunnyliu010@gmail.com>
2024-11-18 20:07:36 -0500

    Add stylings to landing page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -17,7 +17,7 @@ export default async function Posts() {
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">My Blog</h2>
                 </div>
-                <div className="mx-auto mt-6 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-12 border-t border-gray-200 pt-6 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 border-t border-gray-200 pt-6 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {
                         posts.map(({ id, title, description, createdAt}) => {
                             return Card(id, title, description, createdAt)
