0f3c7e97aeaa5f10d6350b8488c09c0f1f8f4e3a
Mouzone <sunnyliu010@gmail.com>
2024-11-20 14:08:35 -0500

    Rewrite username and apssword inputs to use state and setState properties to be able to modify form input

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,4 +1,9 @@
+"use client"
+import React, { useState } from "react";
+
 export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -6,8 +11,8 @@ export default function Login() {
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form action="/api/log-in" method="POST" className="space-y-6">
-                        <Input value="username"/>
-                        <Input value="password"/>
+                        <Input type="username" value={username} update={setUsername}/>
+                        <Input type="password" value={password} update={setPassword}/>
                         <button
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
@@ -29,18 +34,20 @@ function Header() {
     </div>
 }
 
-function Input({value}: { value: string}) {
+function Input({ type, value, update }: { type: string, value: string, update: React.Dispatch<React.SetStateAction<string>> }) {
     return <div>
         <label htmlFor={value} className="block text-sm/6 font-medium text-gray-900">
-            {value.at(0)?.toUpperCase() + value.substring(1)}
+            {type.at(0)?.toUpperCase() + type.substring(1)}
         </label>
         <div className="mt-2">
             <input
-                id={value}
-                name={value}
-                type={value}
+                id={type}
+                name={type}
+                type={type}
+                value={value}
+                onChange={(e) => update(e.target.value)}
                 required
-                autoComplete={value}
+                autoComplete={type}
                 className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm/6"
             />
         </div>
