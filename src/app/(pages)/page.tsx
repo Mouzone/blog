2ebfdb564e60163a5607e72666a18f5ff1beb180
Mouzone <sunnyliu010@gmail.com>
2024-11-21 14:40:55 -0500

    Switch out links

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface Post {
     id: string,
     title: string,
@@ -44,10 +46,10 @@ function Card(id: string, title: string, description: string, dateString: string
         </div>
         <div className="group relative">
             <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                <a href={title}>
+                <Link href={`/${id}`}>
                     <span className="absolute inset-0"/>
                     {title}
-                </a>
+                </Link>
             </h3>
             <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{description}</p>
         </div>
