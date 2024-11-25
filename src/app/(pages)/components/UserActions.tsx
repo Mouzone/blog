fd23ca45bd7f885da6a09126eaf4149642bcb4ab
Mouzone <sunnyliu010@gmail.com>
2024-11-25 12:13:02 -0500

    Change UserActions to use a state to change the options availabel based on if cookies exist

--- src/app/(pages)/components/UserActions.tsx
+++ src/app/(pages)/components/UserActions.tsx
@@ -1,6 +1,6 @@
 "use client"
 import {useRouter} from "next/navigation";
-import React from "react";
+import React, {useState} from "react";
 import {StyledLink} from "@/app/(pages)/[postId]/components/StyledLink.tsx";
 import Cookies from "js-cookie"
 
@@ -8,15 +8,17 @@ export function UserActions() {
     // if authenticated show "new post" and "sign out"
     // if not authenticated show "log-in"
     const router = useRouter()
+    const [loggedIn, setLoggedIn ]= useState(Cookies.get("accessToken") !== null)
 
     return (
-        Cookies.get("accessToken") ? (
+        loggedIn ? (
             <div className="absolute inset-y-0 right-0 flex items-center gap-4 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <StyledLink href="/new-post" text="New Post"/>
                 <button
                     className="text-white bg-purple-900 rounded-md px-3 py-2 text-sm font-medium"
                     onClick={() => {
                         Cookies.remove("accessToken")
+                        setLoggedIn(false)
                         router.push("/")
                     }}
                 >
