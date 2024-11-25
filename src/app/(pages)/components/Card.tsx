9b4af003909822329c2ba952c6919fdc3d66cac7
Mouzone <sunnyliu010@gmail.com>
2024-11-25 14:58:30 -0500

    Add button functionaltiy to each post and get it to render

--- src/app/(pages)/components/Card.tsx
+++ src/app/(pages)/components/Card.tsx
@@ -1,49 +1,45 @@
 import Link from "next/link";
-import Cookies from "js-cookie"
+import CardActions from "@/app/(pages)/components/CardActions.tsx";
 
-export function Card(id: string, title: string, description: string, dateString: string) {
-    const date = new Date(dateString)
+export function Card({
+                         id,
+                         title,
+                         description,
+                         createdAt,
+                         isShown
+                     }: {
+    id: string,
+    title: string,
+    description: string,
+    createdAt: string,
+    isShown: string
+}) {
+    const date = new Date(createdAt);
     const formatted = date.toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'long',
         day: 'numeric'
     })
-    return <article key={id} className="flex max-w-xl flex-col items-start justify-between">
-        <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime={formatted} className="text-gray-500">
-                {formatted}
-            </time>
+
+    return (
+        <div key={id} className="flex">
+            <article className="flex max-w-xl flex-col items-start justify-between">
+                <div className="flex items-center gap-x-4 text-xs">
+                    <time dateTime={formatted} className="text-gray-500">
+                        {formatted}
+                    </time>
+                </div>
+                <div className="group relative">
+                    <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                        <Link href={`/${id}`}>
+                            <span className="absolute inset-0"/>
+                            {title}
+                        </Link>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{description}</p>
+                </div>
+            </article>
+            <CardActions id={id} title={title} description={description} isShown={isShown === "true"}/>
         </div>
-        <div className="group relative">
-            <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                <Link href={`/${id}`}>
-                    <span className="absolute inset-0"/>
-                    {title}
-                </Link>
-            </h3>
-            <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{description}</p>
-        </div>
-        { Cookies.get("accessToken") ||
-            <div>
-                <button>
-                    {/*closed eye*/}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>eye-off-outline</title>
-                        <path
-                            d="M2,5.27L3.28,4L20,20.72L18.73,22L15.65,18.92C14.5,19.3 13.28,19.5 12,19.5C7,19.5 2.73,16.39 1,12C1.69,10.24 2.79,8.69 4.19,7.46L2,5.27M12,9A3,3 0 0,1 15,12C15,12.35 14.94,12.69 14.83,13L11,9.17C11.31,9.06 11.65,9 12,9M12,4.5C17,4.5 21.27,7.61 23,12C22.18,14.08 20.79,15.88 19,17.19L17.58,15.76C18.94,14.82 20.06,13.54 20.82,12C19.17,8.64 15.76,6.5 12,6.5C10.91,6.5 9.84,6.68 8.84,7L7.3,5.47C8.74,4.85 10.33,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C12.69,17.5 13.37,17.43 14,17.29L11.72,15C10.29,14.85 9.15,13.71 9,12.28L5.6,8.87C4.61,9.72 3.78,10.78 3.18,12Z"/>
-                    </svg>
-                    {/*open eye*/}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>eye-outline</title>
-                        <path
-                            d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z"/>
-                    </svg>
-                </button>
-                {/*delete*/}
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title>
-                        <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"/>
-                    </svg>
-                </button>
-            </div>
-        }
-    </article>
+    );
 }
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -17,7 +17,6 @@ export default async function Posts() {
     })
     const data: { posts: Post[] } = await response.json()
     const posts = data["posts"]
-
     return (
         <div className="bg-white py-24 sm:pt-12 pb-24">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -26,9 +25,17 @@ export default async function Posts() {
                 </div>
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 border-t border-gray-200 pt-6 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {
-                        posts.map(({ id, title, description, createdAt}) => {
-                            return Card(id, title, description, createdAt)
-                        })
+                        posts.map(({ id, title, description, createdAt, isShown}) => (
+                            <Card
+                                key={id}
+                                id={id}
+                                title={title}
+                                description={description}
+                                createdAt={createdAt}
+                                isShown={isShown}
+                            />
+                            )
+                        )
                     }
                 </div>
             </div>
