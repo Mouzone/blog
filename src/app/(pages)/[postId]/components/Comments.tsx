a50bf655848d03b2b888960d299be3952a37ee2b
Mouzone <sunnyliu010@gmail.com>
2024-11-26 19:37:28 -0500

    Add pagination to comments

--- src/app/(pages)/[postId]/components/Comments.tsx
+++ src/app/(pages)/[postId]/components/Comments.tsx
@@ -1,3 +1,4 @@
+"use client"
 interface Comment {
     id: string,
     content: string,
@@ -6,11 +7,25 @@ interface Comment {
 }
 import { CreateComment } from './CreateComment.tsx'
 import { Comment } from './Comment.tsx'
+import {useEffect, useState} from 'react'
 
-export default async function Comments({postId}: { postId: string }) {
-    const response = await fetch(`http://localhost:3000/api/comments/${postId}?skip=${0}&take=5`)
-    const data = await response.json()
-    const comments: Comment[] = data["comments"]
+export default function Comments({postId}: { postId: string }) {
+    const [comments, setComments] = useState<Comment[]>([])
+    const [totalComments, setTotalComments] = useState(0)
+    const [skip, setSkip] = useState(0)
+
+    useEffect(() => {
+        async function fetchComments() {
+            const response = await fetch(`http://localhost:3000/api/comments/${postId}?skip=${skip}&take=5`)
+            const data = await response.json()
+
+            setComments(data.comments)
+            setTotalComments(data.totalComments)
+        }
+
+        fetchComments()
+    }, [comments])
+
 
     return (
         <div>
@@ -18,6 +33,24 @@ export default async function Comments({postId}: { postId: string }) {
             {comments.map(comment => (
                 <Comment key={comment.id} comment={comment}/>
             ))}
+            <div className="mx-auto max-w-2xl pt-10 flex justify-between">
+                <button
+                    className="text-white bg-purple-900 rounded-md px-3 py-2 text-sm font-medium
+                                    disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-500"
+                    onClick={() => setSkip(skip - 5)}
+                    disabled={skip === 0}
+                >
+                    Prev
+                </button>
+                <button
+                    className="text-white bg-purple-900 rounded-md px-3 py-2 text-sm font-medium
+                                    disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-500"
+                    onClick={() => setSkip(skip + 5)}
+                    disabled={totalComments <= skip + 5}
+                >
+                    Next
+                </button>
+            </div>
         </div>
     )
 }
