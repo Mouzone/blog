d72c0417f7397417a15a0b139a4d7846f22a3f9b
Mouzone <sunnyliu010@gmail.com>
2024-11-28 15:30:38 -0500

    Fix hydration error for userActions inside navbar

--- src/app/(pages)/components/NavBar.tsx
+++ src/app/(pages)/components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Disclosure } from '@headlessui/react'
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {UserActions} from "@/app/(pages)/components/UserActions.tsx";
 import {StyledLink} from "@/app/(pages)/post/[postId]/components/StyledLink.tsx";
 import {LoginContext} from "@/app/(pages)/components/LoginContextProvider.tsx";
@@ -9,6 +9,14 @@ import Search from "@/app/(pages)/components/Search.tsx";
 
 export default function NavBar() {
     const {accessToken} = useContext(LoginContext)
+    const [isClient, setIsClient] = useState(false)
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    if (!isClient) return null
+
     return (
         <Disclosure as="nav" className="bg-gray-800">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
--- src/app/(pages)/components/Search.tsx
+++ src/app/(pages)/components/Search.tsx
@@ -22,7 +22,10 @@ export default function Search(){
         fetchAccounts()
     }, [accessToken])
 
-    const filteredAccounts = accounts.filter(account => account.username.includes(searchTerm))
+
+    const filteredAccounts = accounts
+        ? accounts.filter(account => account.username.includes(searchTerm))
+        : accounts
     return <div className="relative w-60">
         <input
             id="search"
@@ -34,7 +37,7 @@ export default function Search(){
             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm/6"
         />
         <div className="absolute flex flex-col z-10 w-full bg-white shadow-md max-h-60 overflow-y-auto rounded-md mt-1">
-            {searchTerm && accounts && filteredAccounts.map(account => {
+            {searchTerm && filteredAccounts && filteredAccounts.map(account => {
                 return <Link
                     key={account.id}
                     href={`/account/${account.id}`}
