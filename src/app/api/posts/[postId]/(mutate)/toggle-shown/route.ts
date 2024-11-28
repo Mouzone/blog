a59b629a68c966cb4e6f5d716d5252d25b1723d6
Mouzone <sunnyliu010@gmail.com>
2024-11-27 19:24:23 -0500

    Update toggle-shown route to check for accountId

--- src/app/api/posts/[postId]/(mutate)/toggle-shown/route.ts
+++ src/app/api/posts/[postId]/(mutate)/toggle-shown/route.ts
@@ -1,9 +1,32 @@
-import {updateIsShown} from "../../../../../../../prisma/postQueries.ts";
+import {findPost, updateIsShown} from "../../../../../../../prisma/postQueries.ts";
+import {headers} from "next/headers";
 
 export async function POST(request: Request, { params }: { params: Promise<{ postId: string }> }) {
     try {
+        const headersList = await headers()
+        const accountId = headersList.get("accountId")
+
         const postId = (await params).postId
         const { isShown } = await request.json()
+
+        const post = await findPost(parseInt(postId))
+
+        if (!post) {
+            return Response.json({
+                error: true,
+                status: 403,
+                message: `Invalid post`
+            })
+        }
+
+        if (!accountId || parseInt(accountId) !== post.accountId) {
+            return Response.json({
+                error: true,
+                status: 403,
+                message: `Invalid Credentials`
+            })
+        }
+
         await updateIsShown(parseInt(postId), isShown)
 
         return Response.json({
