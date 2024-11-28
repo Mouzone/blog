7edc5b11b658663c05739cd326fd1767ed6fbc3a
Mouzone <sunnyliu010@gmail.com>
2024-11-28 14:13:27 -0500

    Modify useEffect for search to not update accounts constantly and move posts

--- src/app/(pages)/components/Search.tsx
+++ src/app/(pages)/components/Search.tsx
@@ -18,13 +18,13 @@ export default function Search(){
         }
 
         fetchAccounts()
-    }, [accounts])
+    }, [])
 
     const filteredAccounts = accounts.filter(account => account.username.includes(searchTerm))
     return <div>
         <Input type="search" value={searchTerm} update={setSearchTerm}/>
         <div className="absolute bg-white">
-            {searchTerm && filteredAccounts.map(account => {
+            {searchTerm && accounts && filteredAccounts.map(account => {
                 return <div className="p-2" key={account.id}>
                     {account.username}
                 </div>
