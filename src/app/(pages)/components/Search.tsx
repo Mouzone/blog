865f1e3fd4e300789f0af18ba2145d8e9bbfc4e9
Mouzone <sunnyliu010@gmail.com>
2024-11-28 15:12:03 -0500

    Add placeholder text to search bar

--- src/app/(pages)/components/Search.tsx
+++ src/app/(pages)/components/Search.tsx
@@ -27,6 +27,7 @@ export default function Search(){
             name="search"
             type="search"
             value={searchTerm}
+            placeholder="Search other users"
             onChange={(e) => setSearchTerm(e.target.value)}
             className="block w-full rounded-md border-0 px-2.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm/6"
         />
